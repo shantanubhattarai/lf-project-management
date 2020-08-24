4517ef29ec679c117c5c3c8c2ce4d1f7444ba701
Shantanu Bhattarai <shantanu.bhattarai271@gmail.com>
2020-08-24 21:28:30 +0545

    Add add, update and delete projects to admin

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
 const config = require("../configs/config");
 const router = express.Router();
 const dbConn = require("./../dbconn.js");
 
 router.get("/projects", function (req, res, next) {
-  let sqlQuery = "SELECT * FROM projects";
+  let sqlQuery = "SELECT * FROM projects WHERE is_deleted = 'f'";
 
   dbConn.connection.query(sqlQuery, function (err, result) {
     if (err) {
@@ -24,6 +22,24 @@ router.post("/projects/add", function (req, res, next) {
       return res.send({ status: 400, error: err });
     }
     return res.send({ status: 200, data: result.rows });
+  });
+});
+
+router.delete("/projects/update", function (req, res, next) {
+  let sqlQuery = `update projects set is_deleted='t' where id=${req.body.project_id}`;
+
+  dbConn.connection.query(sqlQuery, function (err, result) {
+    if (err) throw err;
+    res.send({ status: 200, result });
+  });
+});
+
+router.put("/projects/update", function (req, res, next) {
+  let sqlQuery = `update projects SET name=${req.body.name}, description=${req.body.description}, project_manager=${req.body.project_manager_id} where id = ${req.body.project_id}`;
+
+  dbConn.connection.query(sqlQuery, function (err, result) {
+    if (err) throw err;
+    res.send({ status: 200, result });
   });
 });
 
